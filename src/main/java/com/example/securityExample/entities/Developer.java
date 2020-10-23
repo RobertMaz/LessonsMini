8b6cde9d34ce9ed23f6638d33049ce830919940a
RobertMaz <maz.robert@mail.ru>
2020-10-23 09:28:29 +0300

    sec

---
+++ src/main/java/com/example/securityExample/entities/Developer.java
@@ -0,0 +1,12 @@
+package com.example.securityExample.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
